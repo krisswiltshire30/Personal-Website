6f62938fe0d381458d2a87966f862eaa4c7743c9
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2022-10-26 15:30:29 +0100

    check if window is valid

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,23 +3,24 @@ import * as React from "react"
 import Seo from "../components/seo"
 
 const Index = () => {
-  // function reveal() {
-  //   var reveals = document.querySelectorAll(".reveal")
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal")
 
-  //   for (var i = 0; i < reveals.length; i++) {
-  //     var windowHeight = window.innerHeight
-  //     var elementTop = reveals[i].getBoundingClientRect().top
-  //     var elementVisible = 150
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = typeof window !== "undefined" ? window.innerHeight : 0
+      var elementTop = reveals[i].getBoundingClientRect().top
+      var elementVisible = 150
 
-  //     if (elementTop < windowHeight - elementVisible) {
-  //       reveals[i].classList.add("active")
-  //     } else {
-  //       reveals[i].classList.remove("active")
-  //     }
-  //   }
-  // }
-
-  // window.addEventListener("scroll", reveal)
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active")
+      } else {
+        reveals[i].classList.remove("active")
+      }
+    }
+  }
+  if (typeof window !== "undefined") {
+    window.addEventListener("scroll", reveal)
+  }
 
   return (
     <>
