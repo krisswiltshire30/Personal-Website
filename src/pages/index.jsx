cbb5faaf6a08e650f3fa2ec19e1d22bcfba1f66c
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2022-10-26 15:10:20 +0100

    remove window refs- test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,66 +3,65 @@ import * as React from "react"
 import Seo from "../components/seo"
 
 const Index = () => {
+  // function reveal() {
+  //   var reveals = document.querySelectorAll(".reveal")
 
-  function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
+  //   for (var i = 0; i < reveals.length; i++) {
+  //     var windowHeight = window.innerHeight
+  //     var elementTop = reveals[i].getBoundingClientRect().top
+  //     var elementVisible = 150
 
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
+  //     if (elementTop < windowHeight - elementVisible) {
+  //       reveals[i].classList.add("active")
+  //     } else {
+  //       reveals[i].classList.remove("active")
+  //     }
+  //   }
+  // }
 
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
-  }
-
-  window.addEventListener("scroll", reveal);
+  // window.addEventListener("scroll", reveal)
 
   return (
-      <>
-        <Seo title="Homepage" />
-        <section>
-          <h1>Scroll Down to Reveal Elements &#8595;</h1>
-        </section>
-        <section>
-          <div class="container reveal">
-            <h2>Section 1</h2>
-          </div>
-        </section>
+    <>
+      <Seo title="Homepage" />
+      <section>
+        <h1>Scroll Down to Reveal Elements &#8595;</h1>
+      </section>
+      <section>
+        <div class="container reveal">
+          <h2>Section 1</h2>
+        </div>
+      </section>
 
-        <section>
-          <div class="container reveal">
-            <h2>Caption</h2>
-            <div class="text-container">
-              <div class="text-box">
-                <h3>Section Text</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-                  eius molestiae perferendis eos provident vitae iste.
-                </p>
-              </div>
-              <div class="text-box">
-                <h3>Section Text</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-                  eius molestiae perferendis eos provident vitae iste.
-                </p>
-              </div>
-              <div class="text-box">
-                <h3>Section Text</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-                  eius molestiae perferendis eos provident vitae iste.
-                </p>
-              </div>
+      <section>
+        <div class="container reveal">
+          <h2>Caption</h2>
+          <div class="text-container">
+            <div class="text-box">
+              <h3>Section Text</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+                eius molestiae perferendis eos provident vitae iste.
+              </p>
+            </div>
+            <div class="text-box">
+              <h3>Section Text</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+                eius molestiae perferendis eos provident vitae iste.
+              </p>
+            </div>
+            <div class="text-box">
+              <h3>Section Text</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+                eius molestiae perferendis eos provident vitae iste.
+              </p>
             </div>
           </div>
-        </section>
-      </>
+        </div>
+      </section>
+    </>
   )
 }
 
